701538dc286f8be52f57301b25eae3de35e07fbd
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-03 20:04:41 -0800

    added cards for work

--- assests/script/script.js
+++
@@ -1,8 +0,0 @@
-const navBar = document.getElementById("sideNavEl")
-const  workBtn = document.getElementById("sideWork")
-const bioEl = document.getElementById("content")
-
-workBtn.addEventListener("click",function(){
-navBar.style.display = "block"
-});
-
